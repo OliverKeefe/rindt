f18a4f80955ca6d094e5e5a757e28e7bc508a567
oliverkeefe <o@k>
2024-05-07 20:44:51 +0100

    Implement NavbarTop class in Main.java.

--- src/main/java/org/rindt/Main.java
+++ src/main/java/org/rindt/Main.java
@@ -1,15 +1,23 @@
 package org.rindt;
 
 import javax.swing.*;
+import gui.NavbarTop;
+import gui.UIScale;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Rindt");
+        UIScale uiScale = new UIScale();
+        double[] scale = uiScale.getUIScale(8);
+        NavbarTop navbarTop = new NavbarTop(scale);
+        navbarTop.setFont(new Font("SansSerif", Font.PLAIN, 200));
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(1280, 720);
-        mainFrame.pack();
         mainFrame.setVisible(true);
+        mainFrame.setJMenuBar(navbarTop.getJMenuBar());
 
     }
 }
